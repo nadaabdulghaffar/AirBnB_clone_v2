1e5936638ae76eefd32ae1eea2e82a9ceafd55a4
=Nada <nadaabdulghaffar42@gmail.com>
2024-08-05 16:56:00 +0300

    task1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" 
-Starts a Flask web application
+"""
+starts a Flask web application
 """
 
 from flask import Flask
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello():
     """returns Hello HBNB!"""
-    return  "Hello HBNB!"
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
